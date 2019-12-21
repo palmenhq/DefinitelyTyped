a46df8b2e15d0c542494d928ff895f6c0aa192b7
Johan Palmfjord <johan.palmfjord@gmail.com>
2019-12-21 16:54:49 +0100

    Upgrade jsdom version

--- types/jsdom/index.d.ts
+++ types/jsdom/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for jsdom 12.2
+// Type definitions for jsdom 13.2
 // Project: https://github.com/jsdom/jsdom
 // Definitions by: Leonard Thieu <https://github.com/leonard-thieu>
 //                 Johan Palmfjord <https://github.com/palmenhq>
